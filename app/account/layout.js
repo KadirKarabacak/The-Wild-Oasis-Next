e43e001cb8c2ca85ae9ce12743dc7c4c5ebd988e
KadirKarabacak <kadirht@hotmail.com>
2024-09-14 23:37:58 +0300

    feat: send session to side nav as prop

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,9 +1,12 @@
 import SideNavigation from "@/app/_components/SideNavigation";
+import { auth } from "../_lib/auth";
 
-export default function Layout({ children }) {
+export default async function Layout({ children }) {
+    const session = await auth();
+
     return (
-        <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
-            <SideNavigation />
+        <div className="grid grid-cols-[16rem_1fr] max1100:grid-cols-1 h-full gap-12 max800:gap-2">
+            <SideNavigation session={session} />
             <div className="py-1">{children}</div>
         </div>
     );
