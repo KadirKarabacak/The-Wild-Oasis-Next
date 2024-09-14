4c686d8a17abcde1c5708d44b119aec700945b38
KadirKarabacak <kadirht@hotmail.com>
2024-09-14 23:36:35 +0300

    feat: add responsive

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -9,15 +9,15 @@ function ReservationReminder() {
     if (!range.from || !range.to) return null;
 
     return (
-        <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center">
-            <p>
-                <span>👋</span> Don't forget to reserve your dates <br /> from{" "}
+        <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-md bg-accent-500 text-primary-800 font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center max700:w-[60%] max500:w-[80%] max450:w-[95%]">
+            <p className="w-full">
+                👋 Don't forget to reserve your dates from{" "}
                 {format(new Date(range.from), "MMM dd yyyy")} to{" "}
                 {format(new Date(range.to), "MMM dd yyyy")}
             </p>
             <button
                 onClick={resetRange}
-                className="rounded-full p-1 hover:bg-accent-600 transition-all"
+                className="rounded-full p-1 hover:bg-accent-600 transition-all absolute top-1 right-1"
             >
                 <XMarkIcon className="h-5 w-5" />
             </button>
