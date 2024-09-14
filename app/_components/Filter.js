778f5b6c46431f5a1525ac2e9aa88f1817216049
KadirKarabacak <kadirht@hotmail.com>
2024-09-14 23:35:03 +0300

    feat: add responsive

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -61,7 +61,7 @@ function Filter() {
 function Button({ filter, handleFilter, activeFilter, children }) {
     return (
         <button
-            className={`px-5 py-2 hover:bg-primary-700 ${
+            className={`px-5 py-2 max400:px-2 hover:bg-primary-700 ${
                 filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
             }`}
             onClick={() => handleFilter(filter)}
