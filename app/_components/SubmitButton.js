3b5b843bdc165ba4af86b9fda3f0073bb51b0dbc
KadirKarabacak <kadirht@hotmail.com>
2024-09-14 23:37:29 +0300

    feat: add responsive props for submit button

--- app/_components/SubmitButton.js
+++ app/_components/SubmitButton.js
@@ -2,14 +2,14 @@
 import { useFormStatus } from "react-dom";
 import SpinnerMini from "./SpinnerMini";
 
-function SubmitButton({ children, actionText = "Updating..." }) {
+function SubmitButton({ children, actionText = "Updating...", buttonStyles }) {
     // Pending status holds form's current situation
     const { pending, formData, action } = useFormStatus();
 
     return (
         <button
             disabled={pending}
-            className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+            className={`bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 rounded-md disabled:text-gray-300 ${buttonStyles}`}
         >
             {pending ? (
                 <div className="flex gap-2">
