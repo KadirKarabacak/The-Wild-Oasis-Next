e3d6a7caece488a22c73c879c58563e315a99b8a
KadirKarabacak <kadirht@hotmail.com>
2024-09-14 23:37:07 +0300

    feat: add responsive

--- app/_components/SignOutButton.js
+++ app/_components/SignOutButton.js
@@ -6,7 +6,7 @@ function SignOutButton() {
     return (
         <form action={signOutAction}>
             <button className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full">
-                <ArrowRightOnRectangleIcon className="h-5 w-5 text-primary-600" />
+                <ArrowRightOnRectangleIcon className="w-8 h-8 text-primary-300" />
                 <span>Sign out</span>
             </button>
         </form>
