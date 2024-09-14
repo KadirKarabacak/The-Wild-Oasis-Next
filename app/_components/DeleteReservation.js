9dd7996bb245d3e3d944e69839d8d3ba9b48d6e8
KadirKarabacak <kadirht@hotmail.com>
2024-09-14 23:34:56 +0300

    feat: add responsive

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -23,12 +23,13 @@ function DeleteReservation({ bookingId, onDelete }) {
         <button
             disabled={isPending}
             onClick={handleDelete}
-            className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+            className="group max800:justify-center flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
         >
             {!isPending ? (
                 <>
-                    <span className="mt-1">Delete</span>
                     <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+
+                    <span className="mt-1">Delete</span>
                 </>
             ) : (
                 <span className="mx-auto">
