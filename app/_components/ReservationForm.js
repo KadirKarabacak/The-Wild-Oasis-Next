0e3220da1f9e0f3ad4eeff73a50b01bdef55ab86
KadirKarabacak <kadirht@hotmail.com>
2024-09-14 23:36:29 +0300

    feat: add responsive

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -30,7 +30,7 @@ function ReservationForm({ cabin, user }) {
 
     return (
         <div className="scale-[1.01]">
-            <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
+            <div className="bg-primary-800 text-primary-300 px-16 max500:px-4 max500:justify-evenly max400:px-0 py-2 flex justify-between items-center">
                 <p>Logged in as</p>
 
                 <div className="flex gap-4 items-center">
@@ -52,7 +52,7 @@ function ReservationForm({ cabin, user }) {
                     resetRange();
                 }}
                 // action={createBookingWithData}
-                className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col"
+                className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col max500:px-4"
             >
                 <div className="space-y-2">
                     <label htmlFor="numGuests">How many guests?</label>
