a62d9ced9114656aca9d9e6fe1ab84b7c441aa98
KadirKarabacak <kadirht@hotmail.com>
2024-09-16 01:09:37 +0300

    feat: update anchor element with next/link

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,6 +1,7 @@
 import ReservationList from "@/app/_components/ReservationList";
 import { auth } from "@/app/_lib/auth";
 import { getBookings } from "@/app/_lib/data-service";
+import Link from "next/link";
 
 export const metadata = {
     title: "Reservations",
@@ -21,9 +22,9 @@ export default async function Page() {
             {bookings.length === 0 ? (
                 <p className="text-lg">
                     You have no reservations yet. Check out our{" "}
-                    <a className="underline text-accent-500" href="/cabins">
+                    <Link className="underline text-accent-500" href="/cabins">
                         luxury cabins &rarr;
-                    </a>
+                    </Link>
                 </p>
             ) : (
                 <ReservationList bookings={bookings} />
