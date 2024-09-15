1d6647c6c79b29fbffd9fdac2472f32a8f426d2f
KadirKarabacak <kadirht@hotmail.com>
2024-09-16 00:44:10 +0300

    fix: fix login page responsive

--- app/login/page.js
+++ app/login/page.js
@@ -7,8 +7,9 @@ export const metadata = {
 export default function Page() {
     return (
         <div className="flex flex-col gap-10 mt-10 items-center">
-            <h2 className="text-3xl font-semibold">
-                Sign in to access your guest area
+            <h2 className="text-3xl font-semibold max500:text-2xl max400:text-[1.2rem]">
+                Sign in to access your{" "}
+                <span className="text-accent-400">Guest Area</span>
             </h2>
             <SignInButton />
         </div>
