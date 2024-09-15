7c33c8f663c6cb144860d4b7ef37a104692c461b
KadirKarabacak <kadirht@hotmail.com>
2024-09-15 23:31:27 +0300

    feat: add responsive

--- app/account/layout.js
+++ app/account/layout.js
@@ -5,7 +5,7 @@ export default async function Layout({ children }) {
     const session = await auth();
 
     return (
-        <div className="grid grid-cols-[16rem_1fr] max1100:grid-cols-1 h-full gap-12 max800:gap-2">
+        <div className="grid grid-cols-[16rem_1fr] max1100:grid-cols-1 h-full gap-12 max800:gap-6">
             <SideNavigation session={session} />
             <div className="py-1">{children}</div>
         </div>
