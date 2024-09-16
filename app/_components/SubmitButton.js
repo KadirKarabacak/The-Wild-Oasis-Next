43008934b8defef13e50d0e9f0956ab6fff7f59e
KadirKarabacak <kadirht@hotmail.com>
2024-09-17 01:00:16 +0300

    feat: pass props to submit button for loading indicator

--- app/_components/SubmitButton.js
+++ app/_components/SubmitButton.js
@@ -2,16 +2,21 @@
 import { useFormStatus } from "react-dom";
 import SpinnerMini from "./SpinnerMini";
 
-function SubmitButton({ children, actionText = "Updating...", buttonStyles }) {
+function SubmitButton({
+    children,
+    actionText = "Updating...",
+    buttonStyles,
+    isLoading,
+}) {
     // Pending status holds form's current situation
     const { pending, formData, action } = useFormStatus();
 
     return (
         <button
-            disabled={pending}
+            disabled={pending || isLoading}
             className={`bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 rounded-md disabled:text-gray-300 ${buttonStyles}`}
         >
-            {pending ? (
+            {pending || isLoading ? (
                 <div className="flex gap-2">
                     <span>
                         <SpinnerMini />
